66755b2b07f01658d35023c63e157ab873d98899
sourabhservitium <sourabh@servitium.com>
2024-04-24 10:43:11 +0530

    Next 2 questions

---
+++ JavaLibrary/src/main/java/com/example/javalibrary/LeadersOnArray.java
@@ -0,0 +1,31 @@
+package com.example.javalibrary;
+
+public class LeadersOnArray {
+
+    //Leader on array -> There is nothing which is greater than this element on right of it
+    public static void main(String[] args){
+        int[] arr = {7, 10, 4, 3, 6, 5, 2};
+
+        /*for (int i = 0; i < arr.length; i++) {
+            boolean isLeader = true;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] <= arr[j]) {
+                    isLeader = false;
+                    break;
+                }
+            }
+            if (isLeader) {
+                System.out.println(arr[i]);
+            }
+        }*/
+
+        int curr_leader = arr[arr.length - 1];
+        System.out.println(curr_leader);
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (curr_leader < arr[i]) {
+                curr_leader = arr[i];
+                System.out.println(curr_leader);
+            }
+        }
+    }
+}
---
+++ JavaLibrary/src/main/java/com/example/javalibrary/LeftRotateByOne.java
@@ -0,0 +1,17 @@
+package com.example.javalibrary;
+
+public class LeftRotateByOne {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int temp = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = temp;
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
